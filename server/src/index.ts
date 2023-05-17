394213808abfb216966f40cee9565d15e0d2f87a
tantely <ftantely@gmail.com>
2023-05-16 20:44:03 -0600

    3_Connect_To_Database

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,21 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { HelloResolver } from "./resolvers/hello";
 import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { Post } from "./entities/Post";
 
 const main = async () => {
+  //connect to database
+  const conn = await createConnection({
+    type: "postgres",
+    database: "lireddittypeorm",
+    username: "postgres",
+    password: "postgres",
+    logging: true,
+    synchronize: true,
+    entities: [Post],
+  });
+  console.log(conn);
   //create graphql schema
   const schema = await buildSchema({
     resolvers: [HelloResolver],
