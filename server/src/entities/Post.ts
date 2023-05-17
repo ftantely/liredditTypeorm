bd4b2f4594acd743670c064cc298a885c32ab569
tantely <ftantely@gmail.com>
2023-05-16 20:23:04 -0600

    2_Create_Entities

---
+++ server/src/entities/Post.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  title: string;
+}
