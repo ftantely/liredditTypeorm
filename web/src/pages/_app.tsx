eaed351ea64c9946051d5025f159b3afd4070c2c
tantely <ftantely@gmail.com>
2023-06-12 13:53:03 -0600

    19_Urql_Basics

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,22 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-
 import theme from "../theme";
+import { Provider, createClient, cacheExchange, fetchExchange } from "urql";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  exchanges: [cacheExchange, fetchExchange],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
